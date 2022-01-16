ccef1beb5eb37e07a6edb24beabbf2d074037366
Greta Gospodinova <gospodinovagreta@outlook.com>
2022-01-16 16:31:20 +0200

    Initial commit

---
+++ CampringWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CampringWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
